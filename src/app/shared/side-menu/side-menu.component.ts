fb57a5e2f410c39ff7f25493870530873f4d9266
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-19 14:27:30 -0600

    sideMenu li cursor pointer

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -8,6 +8,11 @@ interface MenuItem {
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class SideMenuComponent {
