e4dfb01e3aa6e0ef1bf42ca91f41f8f1d1c18193
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-20 22:45:54 -0600

    Asignando valores por defecto y propiedades allTouched y reset

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   // miFormulario: FormGroup = new FormGroup({
   //   nombre: new FormControl('RTX 4080ti'),
@@ -16,16 +16,34 @@ export class BasicosComponent {
   // })
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(3)]],
-    precio: [0, [Validators.required, Validators.min(0)]],
-    existencias: [0, [Validators.required, Validators.min(0)]]
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]]
   })
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    // Usar setValue limita que tenemos que agregar todos las propiedades #Si es un formulario para nuevo y editar y no tiene un id puede reventar.
+    this.miFormulario.reset({
+      nombre: 'RXT 4080ti',
+      precio: 1600,
+      existencias: 10
+    })
+  }
+
   campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return;
+    };
+    console.log("prueba");
+    this.miFormulario.reset();
   }
 
 }
