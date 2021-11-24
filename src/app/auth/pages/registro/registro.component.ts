1a7ab76cf8c7352cab6889f1788e54e447d02ed6
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-24 10:53:03 -0600

    Refactorizando las validaciones
    Se cambio la posicion de las validaciones auth con el fin de que puedan ser reautilizadas en otros lugares. Estos se pueden importar de dos formas o bien utilizando el archivo de validaciones ts o injectando un servicio

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -9,33 +10,22 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 })
 export class RegistroComponent implements OnInit {
 
-  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
-
-  noPuedeSerRollingTunder(control: FormControl) {
-    const valor: string = control.value?.trim().toLowerCase();
-    if (valor === 'rollingtunder') {
-      return { noRollingTunder: true }
-    }
-    return null;
-  }
-
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [
       Validators.required,
-      Validators.pattern(this.nombreApellidoPattern)
+      Validators.pattern(this.validatorService.nombreApellidoPattern)
     ]],
     email: ['', [
       Validators.required,
-      Validators.pattern(this.emailPattern)
+      Validators.pattern(this.validatorService.emailPattern)
     ]],
     username: ['', [
       Validators.required,
-      this.noPuedeSerRollingTunder
+      this.validatorService.noPuedeSerRollingTunder
     ]]
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
