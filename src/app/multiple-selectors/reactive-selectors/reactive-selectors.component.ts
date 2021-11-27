3d5f69af62fe77fd8af312f04885f17588f23abc
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-27 12:19:13 -0600

    Selectores reactivos funcional

--- src/app/multiple-selectors/reactive-selectors/reactive-selectors.component.ts
+++ src/app/multiple-selectors/reactive-selectors/reactive-selectors.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisesService } from '../services/paises.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/pais';
 
 @Component({
   selector: 'app-reactive-selectors',
@@ -8,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveSelectorsComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    continente: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
+  })
+
+  continentes: string[] = []
+  paises: Pais[] = []
+  fronteras: PaisSmall[] = []
+
+  constructor(private fb: FormBuilder, private paisesService: PaisesService) { }
 
   ngOnInit(): void {
+    this.continentes = this.paisesService.continentes;
+    this.miFormulario.get('continente')?.valueChanges
+      .pipe(
+        // tap(() => this.miFormulario.get('pais')?.reset('')),
+        switchMap(continente => this.paisesService.getPaises(continente)))
+      .subscribe(paises => this.paises = paises);
+    this.miFormulario.get('pais')?.valueChanges
+      .pipe(
+        // tap(() => console.log("Pase por aqui")),
+        switchMap(cca2 => this.paisesService.getBordersConCca2(cca2)),
+        switchMap(({ borders }) => this.paisesService.getPaisesFronteras(borders!))
+      )
+      .subscribe(fronteras => this.fronteras = fronteras)
   }
 
+  guardar() {
+    console.log(this.miFormulario.value)
+  }
 }
